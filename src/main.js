471e9090e69694651808d8d2efe7633a5a5f63a2
darkshadowknight <ayenndeveraa@gmail.com>
2025-05-07 19:51:29 +0800

    Adding project files

---
+++ src/main.js
@@ -0,0 +1,49 @@
+let imgsarry = [
+    "src_pic/bwershapic/1.jpg",
+    "src_pic/bwershapic/2.jpg",
+    "src_pic/bwershapic/3.jpg",
+    "src_pic/bwershapic/4.jpg",
+    "src_pic/bwershapic/5.jpg",
+    "src_pic/bwershapic/6.jpg",
+    "src_pic/bwershapic/7.jpg"
+  ];
+  
+  let imgElements = [
+    document.getElementById("img1"),
+    document.getElementById("img2"),
+    document.getElementById("img3"),
+    document.getElementById("img4"),
+    document.getElementById("img5")
+  ];
+  
+  let startIndex = 0;
+  
+  function updateImages() {
+    for (let i = 0; i < imgElements.length; i++) {
+      let img = imgElements[i];
+      let imgIndex = (startIndex + i) % imgsarry.length;
+  
+      img.classList.remove("show");
+      img.classList.add("fade");
+
+      setTimeout(() => {
+        img.src = imgsarry[imgIndex];
+  
+        img.classList.add("show");
+      }, 100);
+    }
+  }
+  
+  
+  updateImages();
+  
+  function prevbt() {
+    startIndex = (startIndex - 1 + imgsarry.length) % imgsarry.length;
+    updateImages();
+  }
+  
+  function nextbt() {
+    startIndex = (startIndex + 1) % imgsarry.length;
+    updateImages();
+  }
+  
